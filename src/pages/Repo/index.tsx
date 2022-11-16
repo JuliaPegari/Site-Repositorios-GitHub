3adb6d6f7c2d0f63817ab53c961f86f8922fd065
Júlia Pereira Galdiano Ribeiro <50510515+JuliaPegari@users.noreply.github.com>
2022-11-16 20:39:40 -0300

    second commit

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header } from './styles';
+import logo from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 export const Repo: React.FC = () => {
-  return <h1>Repositorios!</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitCollection" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+    </>
+  );
 };

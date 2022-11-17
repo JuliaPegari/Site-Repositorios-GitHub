51a3967141531ccb127fad656ccd19873cb6fbd8
Júlia Pereira Galdiano Ribeiro <50510515+JuliaPegari@users.noreply.github.com>
2022-11-17 02:16:09 -0300

    third commit

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronsRight, FiChevronLeft } from 'react-icons/fi';
 
-import { Header } from './styles';
+import { api } from '../../services/api';
+import { Header, RepoInfo, Issues } from './styles';
 import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface GithubIssues {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 export const Repo: React.FC = () => {
+  const [repository, setRepository] = React.useState<GithubRepository | null>(
+    null,
+  );
+  const [issues, setIssues] = React.useState<GithubIssues[]>([]);
   const { params } = useRouteMatch<RepositoryParams>();
+
+  React.useEffect(() => {
+    api
+      .get(`repos/${params.repository}`)
+      .then(response => setRepository(response.data));
+  }, [params.repository]);
+
+  React.useEffect(() => {
+    api
+      .get(`repos/${params.repository}/issues`)
+      .then(response => setIssues(response.data));
+  }, [params.repository]);
 
   return (
     <>
@@ -21,6 +59,47 @@ export const Repo: React.FC = () => {
           Voltar
         </Link>
       </Header>
+      {repository && (
+        <RepoInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues Abertas</span>
+            </li>
+          </ul>
+        </RepoInfo>
+      )}
+      ;
+      <Issues>
+        {issues.map(issue => (
+          <a href={issue.html_url} key={issue.id}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronsRight size={20} />
+          </a>
+        ))}
+      </Issues>
     </>
   );
 };
